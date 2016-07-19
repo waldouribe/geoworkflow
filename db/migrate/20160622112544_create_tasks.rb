2ed081f427f378ac71dd75e52325cdf7d53443b3
Waldo Uribe <wuribe@gmail.com>
2016-07-19 17:28:21 -0400

    remove foreign key from my taks users

--- db/migrate/20160622112544_create_tasks.rb
+++ db/migrate/20160622112544_create_tasks.rb
@@ -2,8 +2,8 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.references :my_process, index: true, foreign_key: true
-      t.references :user, index: true, foreign_key: true
-      t.references :responsible_user, index: true, foreign_key: true
+      t.references :user, index: true
+      t.references :responsible_user, index: true
       t.string :address
       t.string :name
       t.text :description

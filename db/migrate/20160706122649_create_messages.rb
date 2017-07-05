6d03d5034cda28f616194bc60619ffbf66721ddc
Waldo Uribe <uribe.fache@gmail.com>
2017-07-04 23:01:58 -0400

    sender y receiver no son refenreces, son integer y rails despues lo asocia a un usuario

--- db/migrate/20160706122649_create_messages.rb
+++ db/migrate/20160706122649_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.references :sender, index: true, foreign_key: true
-      t.references :receiver, index: true, foreign_key: true
+      t.integer :sender_id, index: true
+      t.integer :receiver_id, index: true
       t.references :my_process, index: true, foreign_key: true
       t.text :message
 

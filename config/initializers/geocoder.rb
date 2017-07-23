441d595ae91efa538bf4287b1c3b5bffa6a95f6f
Waldo Uribe <uribe.fache@gmail.com>
2017-07-23 17:15:04 -0400

    rails g geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,16 +1,21 @@
 Geocoder.configure(
+  # Geocoding options
+  timeout: 3,                 # geocoding service timeout (secs)
+  lookup: :google,            # name of geocoding service (symbol)
+  # language: :en,              # ISO-639 language code
+  # use_https: false,           # use HTTPS for lookup requests? (if supported)
+  # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
+  # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
+  api_key: "AIzaSyCQLHznYx6UKpxcAAsWKXc2vMyL_TRRV6g",               # API key for geocoding service
+  # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
+  # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
-  # geocoding service (see below for supported options):
-  :lookup => :google,
+  # Exceptions that should not be rescued by default
+  # (if you want to implement custom error handling);
+  # supports SocketError and Timeout::Error
+  # always_raise: [],
 
-  # IP address geocoding service (see below for supported options):
-
-  # to use an API key:
-  :api_key => "AIzaSyCQLHznYx6UKpxcAAsWKXc2vMyL_TRRV6g",
-
-  # geocoding service request timeout, in seconds (default 3):
-  :timeout => 5,
-
-  # set default units to kilometers:
-  :units => :km
+  # Calculation options
+  # units: :mi,                 # :km for kilometers or :mi for miles
+  # distances: :linear          # :spherical or :linear
 )

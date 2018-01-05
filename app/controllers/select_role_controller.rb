d0cdd5f8fb8ddbda7767f62976715f7afe5d093b
Waldo Uribe <uribe.fache@gmail.com>
2018-01-04 22:37:42 -0300

    geocode new

--- app/controllers/select_role_controller.rb
+++ app/controllers/select_role_controller.rb
@@ -4,10 +4,15 @@ class SelectRoleController < ApplicationController
 
   def update
     if current_user.role != 'not-admited'
+
       current_user.ip = request.ip
       current_user.role = params[:select_role][:role]
+      location = IpGeocoder.geocode(request.ip)
+      puts(location)
+      render text: location;return
       current_user.save
     end
+    render text: request.location.to_yaml;return
     redirect_to root_path
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     sign_in(user)
-    render text: request.location;return
     redirect_to select_role_path
   end
 
